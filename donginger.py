c21e407014f8a47c15182633182bd35a086f95cf
Sensoz <sensoz@gmail.com>
2011-10-14 14:42:43 +0700

    Dummy processor class, simple DB class added

--- donginger.py
+++ donginger.py
@@ -2,6 +2,7 @@
 
 import sys
 import telnetlib
+import sqlite3 as sqlite
 from xml.etree.ElementTree import ElementTree
 
 CONFIG = 'donginger.xml'
@@ -17,13 +18,25 @@ def parseConf():
 		conf['username'] = tree.find('username').text
 		conf['password'] = tree.find('password').text
 		conf['host'] = tree.find('host').text
+		conf['first_command'] = tree.find('first_command').text
 		
 		return conf
 	except:
 		print 'Error parsing configuration.'
 		sys.exit(1)
 		
-class Connector:
+		
+class DB:
+	def __init__(self):
+		if os.path.exists(conf['main_db']):
+			self.cx = sqlite.connect(conf['main_db'], isolation_level=None)
+			self.cu = self.cx.cursor()
+		else:
+			print "Database not found, quitting."
+			sys.exit(1)
+		
+		
+class TelnetConnector:
 	def __init__(self):
 		self.tn = None
 	
@@ -31,7 +44,8 @@ class Connector:
 		self.tn = telnetlib.Telnet(conf['host'], 7777)
 		self.tn.read_until(" connected)")
 		self.tn.write("connect %s %s\n" % (conf['username'], conf['password']))
-		self.tn.write("@join dionysus\n")
+		if conf['first_command']:
+			self.write(conf['first_command'])
 
 	def write(self, str):
 		if self.tn:
@@ -39,12 +53,19 @@ class Connector:
 		else:
 			print 'No running telnet connection.'
 
+
+class Processor:
+	def __init__(self, con):
+		self.con = con
+	
+	def parser(self):
+		self.processLine(self.con.read_until("\n"))
+
 if __name__ == '__main__':
 	print "Starting up..."
 	
-	con = Connector()
-	proc = Processor(con)
-	twit = Twit()
+	con = TelnetConnector()
+	db = DB()
 
 	while True:
 		try:
