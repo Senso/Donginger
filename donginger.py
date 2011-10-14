26258c8b1667193290283fd260c934b3846d919c
Sensoz <sensoz@gmail.com>
2011-10-14 16:20:14 +0700

    Fixes + import twit.py

--- donginger.py
+++ donginger.py
@@ -7,13 +7,18 @@ import telnetlib
 import sqlite3 as sqlite
 from xml.etree.ElementTree import ElementTree
 
-CONFIG = 'donginger.xml'
-conf = {}
+# Local imports
+from twit import Twit
+
+CONFIG = 'donginger.conf'
 
 class Dong(object):
 	pass
 
 def parseConf():
+	dong.modules = {}
+	dong.commands = {}
+	
 	try:
 		dong.config = json.load(open(CONFIG, 'r'))
 		
@@ -21,31 +26,31 @@ def parseConf():
 		print 'Error parsing configuration:', e
 		sys.exit(1)
 		
-	for name, conf in dong.config['modules']:
-		dong.modules[name] = conf
-		dong.commands[conf['command']] = name
-		
-		
+	for i in dong.config['modules'].items():
+		dong.modules[i[0]] = i[1]
+		dong.commands[i[1]['command']] = i[0]
+
+	
 class DB:
 	def __init__(self):
-		if os.path.exists(conf['main_db']):
-			self.cx = sqlite.connect(conf['main_db'], isolation_level=None)
+		if os.path.exists(dong.config['database']):
+			self.cx = sqlite.connect(dong.config['database'], isolation_level=None)
 			self.cu = self.cx.cursor()
 		else:
 			print "Database not found, quitting."
 			sys.exit(1)
-		
-		
+
+
 class TelnetConnector:
 	def __init__(self):
 		self.tn = None
 	
 	def connect(self):
-		self.tn = telnetlib.Telnet(conf['host'], 7777)
+		self.tn = telnetlib.Telnet(dong.config['host'], 7777)
 		self.tn.read_until(" connected)")
-		self.write("connect %s %s" % (conf['username'], conf['password']))
-		if conf['first_command']:
-			self.write(conf['first_command'])
+		self.write("connect %s %s" % (dong.config['username'], dong.config['password']))
+		if dong.config['first_command']:
+			self.write(dong.config['first_command'])
 
 	def write(self, str):
 		if self.tn:
