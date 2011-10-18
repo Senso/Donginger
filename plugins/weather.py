c2eeb1636436caf1fd6aa4e8cd43ada2d4dfdb70
Sensoz <sensoz@gmail.com>
2011-10-18 17:23:50 +0700

    adding basic functionality

--- plugins/weather.py
+++ plugins/weather.py
@@ -10,8 +10,8 @@ class Weather:
 		self.conf = self.dong.plugins_conf[name]
 		
 	def build_query(self, url, params):
-		post_data = urlencode(params)
-		request = urllib2.Request(url, post_data)
+		query_string = urlencode(params)
+		request = urllib2.Request(url + '?' + query_string)
 		opener = urllib2.build_opener()
 		return opener.open(request)
 		
@@ -28,7 +28,22 @@ class Weather:
 		self.process_xml(xml)
 		
 	def process_xml(self, xml):
-		if not xml:
+		if xml is None:
 			return None
 		
-		print xml
+		xml = xml.find('weather')
+		
+		info = dict((e.tag, e.get('data')) for e in w.find('current_conditions'))
+		info['city'] = xml.find('forecast_information/city').get('data')
+		info['high'] = w.find('forecast_conditions/high').get('data')
+		info['low'] = w.find('forecast_conditions/low').get('data')
+		
+		try:
+			return '%(city)s: %(condition)s, %(temp_f)sF/%(temp_c)sC (H:%(high)sF'\
+					', L:%(low)sF), %(humidity)s, %(wind_condition)s.' % info
+		except:
+			pass
+		
+		
+		
+		
