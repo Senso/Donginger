6e7ac44740eff42d418e74e2676ab843ff5e3d60
Sensoz <sensoz@gmail.com>
2011-10-20 10:25:19 +0700

    PEP 8

--- plugins/suggest.py
+++ plugins/suggest.py
@@ -12,19 +12,19 @@ class Twit(Plugin):
 		page_json = page.split('(', 1)[1][:-1]
 		suggestions = json.loads(page_json)[1]
 		if suggestions:
-			suggestions = self.removeLyrics(suggestions)
+			suggestions = self.remove_lyrics(suggestions)
 			random_sug = random.choice(suggestions)
-			self.storeSug(who, inp)
+			self.store_suggestion(who, inp)
 			return random_sug
 			
-	def removeLyrics(self, sug):
+	def remove_lyrics(self, sug):
 		nyt = []
 		for s in sug:
 			if s[0].find('lyrics') > -1: pass
 			else: nyt.append(s)
 		return nyt
 	
-	def storeSug(self, sug):
+	def store_suggestion(self, sug):
 		sug = sug.strip('\r')
 		sug = sug.strip()
 		
@@ -34,7 +34,7 @@ class Twit(Plugin):
 			# UNIQUE constraint will balk here
 			pass
 		
-	def pullSug(self):
+	def pull_suggestion(self):
 		random_sug = self.dong.db.get_random_row('suggest')
 		res = self.GoogleSuggest(random_sug)
 		return res
