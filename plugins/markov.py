23875989281177e3aa7bca254981219cb57572d6
Sensoz <sensoz@gmail.com>
2011-10-20 16:43:23 +0700

    trying to make it work with multiple bodies of text

--- plugins/markov.py
+++ plugins/markov.py
@@ -5,7 +5,7 @@ import random
 from plugin import Plugin
 
 class Markov(Plugin, collections.defaultdict):
-	"""	Markov Chain test, taken from
+	"""	Markov Chain plugin, taken from
 		http://www.evanfosmark.com/2009/11/python-markov-chains-and-how-to-use-them/
 	"""
 	
@@ -16,31 +16,49 @@ class Markov(Plugin, collections.defaultdict):
 		super(Markov, self).__init__(name, dong)
 		collections.defaultdict.__init__(self, DynamicDie)
 		
-	def add(self, iterable):
+		self.texts = self.conf['texts']
+		self.chains = {}
+		self.process_all_texts()
+		
+	def add(self, chain, iterable):
 		""" Insert an iterable (pattern) item into the markov chain.
 			The order of the pattern will define more of the chain.
 		"""
-		item1 = item2 = MarkovChain.START
+		item1 = item2 = Markov.START
 		for item3 in iterable:
-			self[(item1, item2)].add_side(item3)
+			self.chains[chain][(item1, item2)].add_side(item3)
 			item1 = item2
 			item2 = item3
-		self[(item1, item2)].add_side(MarkovChain.END)
+		self.chains[chain][(item1, item2)].add_side(Markov.END)
 
-	def random_output(self, max=100):
+	def random_output(self, chain, max=100):
 		""" Generate a list of elements from the markov chain.
 			The `max` value is in place in order to prevent excessive iteration.
 		"""
 		output = []
-		item1 = item2 = MarkovChain.START
+		item1 = item2 = Markov.START
 		for i in range(max-2):
-			item3 = self[(item1, item2)].roll()
-			if item3 is MarkovChain.END:
+			item3 = self.chains[chain][(item1, item2)].roll()
+			if item3 is Markov.END:
 				break
 			output.append(item3)
 			item1 = item2
 			item2 = item3
 		return output
+	
+	def process_all_texts(self):
+		for name,file in self.texts.items:
+			self.add_body(name, file)
+	
+	def add_body(self, name, file):
+		self.chains[name] = {}
+		for line in file:
+			line = line.split()
+			for word in line:
+				word = word.strip()
+				if word:
+					self.add(name, word)
+		
 		
 class DynamicDie(collections.defaultdict):
 
