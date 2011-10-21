29621d41b9f947d891a615c435c8c3a094bbb26d
Sensoz <sensoz@gmail.com>
2011-10-21 10:06:41 +0700

    added new mem module

---
+++ plugins/mem.py
@@ -0,0 +1,31 @@
+
+# Taken from skybot (https://github.com/rmmh/skybot/)
+
+import os
+import re
+
+from plugin import Plugin
+
+class Mem(Plugin):
+	def __init__(self, name, dong):
+		super(Mem, self).__init__(name, dong)
+		
+	def show_mem(self, callback, who, cmd):
+		"""Returns the bot's current memory usage."""
+		
+		if os.name == 'posix':
+			status_file = open("/proc/%d/status" % os.getpid()).read()
+			line_pairs = re.findall(r"^(\w+):\s*(.*)\s*$", status_file, re.M)
+			status = dict(line_pairs)
+			keys = 'VmSize VmLib VmData VmExe VmRSS VmStk'.split()
+			return ', '.join(key + ':' + status[key] for key in keys)
+	
+		elif os.name == 'nt':
+			cmd = "tasklist /FI \"PID eq %s\" /FO CSV /NH" % os.getpid()
+			out = os.popen(cmd).read()
+	
+			total = 0
+			for amount in re.findall(r'([,0-9]+) K', out):
+				total += int(amount.replace(',', ''))
+	
+			return 'memory usage: %d kB' % total
