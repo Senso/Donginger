68aceaedb520f744187e3cf219b49b46f6938412
Sensoz <sensoz@gmail.com>
2011-10-21 12:00:05 +0700

    adding get_json() method

--- plugin.py
+++ plugin.py
@@ -1,4 +1,5 @@
 
+import json
 import urllib2
 from urllib import urlencode
 
@@ -9,8 +10,14 @@ class Plugin(object):
 		self.dong = dong
 		self.conf = self.dong.plugins_conf[name]
 		
-	def build_query(self, url, params):
-		query_string = urlencode(params)
-		request = urllib2.Request(url + '?' + query_string)
+	def build_query(self, url, params=None):
+		if params:
+			query_string = urlencode(params)
+			request = urllib2.Request(url + '?' + query_string)
+		else:
+			request = urllib2.Request(url)
 		opener = urllib2.build_opener()
 		return opener.open(request)
+		
+	def get_json(self, url, params=None):
+		return json.loads(self.build_query(url, params).read())
