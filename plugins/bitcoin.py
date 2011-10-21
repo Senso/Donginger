843023fac17ae5028405237975d9a447d2f9af2e
Sensoz <sensoz@gmail.com>
2011-10-21 13:51:19 +0700

    missing blackslash

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -12,5 +12,5 @@ class Bitcoin(Plugin):
 		
 		data = self.get_json("https://mtgox.com/code/data/ticker.php")
 		ticker = data['ticker']
-		return "Current: \x0307$%(buy).2f\x0f - High: \x0307$%(high).2f\x0f"
+		return "Current: \x0307$%(buy).2f\x0f - High: \x0307$%(high).2f\x0f" \
 			" - Low: \x0307$%(low).2f\x0f - Volume: %(vol)s" % ticker
