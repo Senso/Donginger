72db128f1332f5d163a1672bc4a735cd45e7e14c
Sensoz <sensoz@gmail.com>
2011-10-25 10:05:11 +0700

    fixed missing argument bug

--- plugins/suggest.py
+++ plugins/suggest.py
@@ -7,17 +7,17 @@ class Suggest(Plugin):
 	def __init__(self, name, dong):
 		super(Suggest, self).__init__(name, dong)
 		
-	def google_suggest(self, callback, who, str):
+	def google_suggest(self, callback, who, arg):
 		"""<suggest string> - returns a random Google Suggestion based on the string."""
 		
-		w = self.build_query('http://google.com/complete/search', {'q': str})
+		w = self.build_query('http://google.com/complete/search', {'q': arg})
 		page = w.read()
 		page_json = page.split('(', 1)[1][:-1]
 		suggestions = json.loads(page_json)[1]
 		if suggestions:
 			suggestions = self.remove_lyrics(suggestions)
 			random_sug = choice(suggestions)
-			self.store_suggestion(who, str)
+			self.store_suggestion(who, arg)
 			return random_sug
 			
 	def remove_lyrics(self, sug):
@@ -37,7 +37,7 @@ class Suggest(Plugin):
 			# UNIQUE constraint will balk here
 			pass
 		
-	def pull_suggestion(self, who, arg):
+	def pull_suggestion(self, callback, who, arg):
 		"""Nudging the bot makes it return a random Google suggestion."""
 		
 		random_sug = self.dong.db.get_random_row('suggest')
