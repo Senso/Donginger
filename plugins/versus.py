d92f6c6c371dc5e30201e2429d187bc8aa14f45a
Sensoz <sensoz@gmail.com>
2011-11-18 14:20:47 +0700

    fixed bug

--- plugins/versus.py
+++ plugins/versus.py
@@ -1,17 +1,19 @@
 # This plugin requires the Rand plugin to work.
 
+import re
 from plugin import Plugin
 
 class Versus(Plugin):
 	def __init__(self, dong, conf):
 		super(Versus, self).__init__(dong, conf)
+		self.dong = dong
 		
 	def versus(self, callback, who, arg):
 		"""<player> vs <player> - returns random quotes from two players."""
 		
 		opps = re.search('(.+) vs (.+)', callback, re.I)
 		if opps:
-			rand = dong.plugins['rand']
+			rand = self.dong.plugins['rand']
 			dude1 = rand.random_line(callback, who, opps.group(1))
 			dude2 = rand.random_line(callback, who, opps.group(2))
 			if dude1 and dude2:
