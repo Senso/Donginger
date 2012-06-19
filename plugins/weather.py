3bf73ae085da20abd4ac92a737ba490fc31d6cc8
Sensoz <sensoz@gmail.com>
2012-06-19 10:25:28 +0700

    now using Weather Underground

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,12 +1,10 @@
-
-# Taken from skybot (https://github.com/rmmh/skybot/)
-
 from plugin import Plugin
-from lxml import etree
 
-class Weather(Plugin):
+class Wu(Plugin):
+	""" Weather Underground plugin. """
+	
 	def __init__(self, dong, conf):
-		super(Weather, self).__init__(dong, conf)
+		super(Wu, self).__init__(dong, conf)
 		
 	def fetch_weather(self, callback, who, loc):
 		"""<weather location> returns the weather for that location. Not providing a location makes it returns the weather of your last used location."""
@@ -28,32 +26,14 @@ class Weather(Plugin):
 			else:
 				self.dong.db.insert('weather', {'user': who, 'location': loc})
 		
-		xml = self.get_xml('http://www.google.com/ig/api', {'weather': loc})
-		# Strip funky UTF8 characters
-		return self.process_xml(xml)
-		
-	def process_xml(self, xml):
-		if xml is None:
-			return None
-		
-		xml = xml.find('weather')
-		
-		if xml.find('problem_cause') is not None:
-			return None
-		
 		try:
-			info = dict((e.tag, e.get('data')) for e in xml.find('current_conditions'))
-			info['city'] = xml.find('forecast_information/city').get('data')
-			info['high'] = xml.find('forecast_conditions/high').get('data')
-			info['low'] = xml.find('forecast_conditions/low').get('data')
-			
-			output = '%(city)s: %(condition)s, %(temp_f)sF/%(temp_c)sC (H:%(high)sF'\
-					', L:%(low)sF), %(humidity)s, %(wind_condition)s.' % info
+			results = self.get_json("http://api.wunderground.com/api/%s/geolookup/conditions/forecast/q/%s.json" %
+								(self.conf['weather_underground_key'], loc))
 		except:
-			return None
+			# Most likely not JSON
+			return
 		
-		return output.encode('ascii', 'ignore')
-		
-		
-		
-		
+		return results
+		#xml = self.get_json('http://www.google.com/ig/api', {'weather': loc})
+		# Strip funky UTF8 characters
+		#return self.process_xml(xml)
