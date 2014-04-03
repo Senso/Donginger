680c21e85ff625369e420ce3cfcf1e679cc02b5c
Senso <sensoz@gmail.com>
2014-04-03 12:09:23 -0700

    bug fixes

--- plugin.py
+++ plugin.py
@@ -10,7 +10,8 @@ class Plugin(object):
 		self.dong = dong
 		self.conf = conf
 		
-	def build_query(self, url, params=None):
+	def build_query(self, url, params=None, get_method=None):
+
 		if params:
 			query_string = urlencode(params)
 			if url.find('?') > -1:	
@@ -19,14 +20,28 @@ class Plugin(object):
 				request = urllib2.Request(url + '?' + query_string)
 		else:
 			request = urllib2.Request(url)
+
+		if get_method is not None:
+			request.get_method = lambda: get_method
+
 		opener = urllib2.build_opener()
 		return opener.open(request)
-		
-	def get_html(self, url, params=None):
-		return html.fromstring(self.build_query(url, params).read())
+
+	def get_head(self, url):
+		return self.build_query(url, get_method='HEAD').read()
+
+	def get_html(self, url, params=None, get_method=None):
+		return html.fromstring(self.build_query(url, params, get_method).read())
 	
 	def get_json(self, url, params=None):
 		return json.loads(self.build_query(url, params).read())
+
+	def get_json_https(self, url, params=None):
+		httpsha = urllib2.HTTPSHandler()
+		opener = urllib2.build_opener(httpsha)
+		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		uobj = opener.open(url)
+		return json.loads(uobj.read())
 		
 	def get_xml(self, url, params=None):
 		return (etree.fromstring(self.build_query(url, params).read()))
